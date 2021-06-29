f3dd04f2dfd68eaf89158bf0cf73f3af58220a12
suey0128 <syui0128@gmail.com>
2021-06-28 23:24:13 -0700

    commit tree ec66dd3bbf6f22b2c2a0b694f89e5514d8efc6b7

--- lib/min.rb
+++ lib/min.rb
@@ -1,13 +1,27 @@
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_min method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+groceries = {
+ dairy: ["milk", "yogurt", "cheese"],
+ vegetable: ["carrots", "broccoli", "cucumbers"],
+ meat: ["chicken", "steak", "salmon"],
+ grains: ["rice", "pasta"]
+}
 
 def get_the_min(groceries)
-  #code your solution here!
+
+  # new_arr = []
+  # nested_value_arr = groceries.values
+
+  # row_inex = 0
+  # while row_inex < nested_value_arr.length do
+  #   elem_index = 0
+  #   while elem_index < nested_value_arr[row_inex].length do
+  #     new_arr << nested_value_arr[row_inex][elem_index]
+  #     elem_index += 1
+  #   end
+  # row_inex += 1
+  # end
+  # new_arr.min
+  groceries.values.flatten.min
 end
--- lib/values.rb
+++ lib/values.rb
@@ -1,13 +1,27 @@
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_values method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+groceries = {
+ dairy: ["milk", "yogurt", "cheese"],
+ vegetable: ["carrots", "broccoli", "cucumbers"],
+ meat: ["chicken", "steak", "salmon"],
+ grains: ["rice", "pasta"]
+}
 
 def get_the_values(groceries)
-  #code your solution here!
+  # new_arr = []
+  # # groceries.values.each{|elem| new_arr << elem}
+  # nested_value_arr = groceries.values
+
+  # row_inex = 0
+  # while row_inex < nested_value_arr.length do
+  #   elem_index = 0
+  #   while elem_index < nested_value_arr[row_inex].length do
+  #     new_arr << nested_value_arr[row_inex][elem_index]
+  #     elem_index += 1
+  #   end
+  # row_inex += 1
+  # end
+  # new_arr
+  groceries.values.flatten
 end
